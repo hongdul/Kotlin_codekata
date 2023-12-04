5cd0c89ce528324382172f75527fc075c412a01c
hongdul <kkw0426@naver.com>
2023-12-04 22:53:29 +0900

    commit codekata1.kt, codekata4.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
+    class Solution {
+        fun solution(age: Int): Int {
+            var answer: Int = 0
+            answer = 2023 - age
+            return answer
+        }
+    }
+// Command + /
+// Cmd + 9  Git 체크
+// Cmd + k  commit 창 열기
+//
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+//    println("Program arguments: ${args.joinToString()}")
 }
