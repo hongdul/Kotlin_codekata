a40b217c9214beac2d9a970cb5c67dddf7c1609e
hongdul <kkw0426@naver.com>
2023-12-05 09:57:04 +0900

    commit codekata2.kt, codekata3.kt

---
+++ src/main/kotlin/codekata3.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    class Solution  {
+        fun solution(num1: Int, num2: Int): Int {
+            var answer: Int = 0
+         return num1 / num2
+        }
+    }
+}
